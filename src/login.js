6bf5235b65a272f369518c478efa5e0ea8137680
saurav kumar <64458729+serz4862@users.noreply.github.com>
2024-03-19 20:56:15 +0530

    login page

--- src/login.js
+++ src/login.js
@@ -40,7 +40,7 @@ const Login = () => {
                 type="password"
                 autoComplete="current-password"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-white rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm bg-gray-900"
+                className="appearance-none rounded-none relative block w-full mt-2 px-3 py-2 border  border-gray-300 placeholder-gray-500 text-white rounded-b-m focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm bg-gray-900"
                 placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -54,6 +54,18 @@ const Login = () => {
             >
               Log in
             </button>
+            <div>
+          <h5 className="mt-1 text-center text-1.5xl font-extrabold text-white"> OR</h5>
+        </div>
+
+            <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center mt-2 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Signup
+              </button>
+            </div>
           </div>
         </form>
       </div>
